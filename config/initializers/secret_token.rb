d37e2cb48ffe9eec98e44de6f35064fe0261c98a
Jatin Naik <jsnaik@gmail.com>
2014-01-29 23:22:30 +0530

    Created a rails app without db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TemperatureConverter::Application.config.secret_key_base = '75781cc154d90da948b9a9df3550259bfbb7600ff303af94e508e9f2cac1c4e3821a365d5ef6ea2b038d99001f1ace58b0ff4e755d6d1f1f12efa5ac7c6a1031'
